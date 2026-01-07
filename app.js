7d2f7369067f3a582e0db26b52fe1e918ec6d507
Agam00 <agamxpro69@gmail.com>
2026-01-07 08:28:20 +0530

    Added Reviews Section

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 // mongo url
 const MONGO_URL = "mongodb://127.0.0.1:27017/StayScape";
@@ -44,6 +45,15 @@ const validateListing = (req, res, next) => {
     next();
   }
 };
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
 // show all listings route
 app.get(
   "/listings",
@@ -63,7 +73,7 @@ app.get(
   "/listings/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing });
   })
 );
@@ -110,6 +120,38 @@ app.delete(
     res.redirect("/listings");
   })
 );
+
+//reviews
+
+// Post Review route
+app.post(
+  "/listings/:id/reviews",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+    await listing.save();
+    await newReview.save();
+
+    res.redirect(`/listings/${listing._id}`);
+  })
+);
+
+// Delete Review route
+
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+  })
+);
+
 // root route
 app.get("/", (req, res) => {
   res.redirect("/listings");
